c022dda2165de3025f54734690ca19f368f63e48
sachith95 <sachithsilva2012@gmail.com>
2020-02-09 14:05:17 +0800

    xcode 10.3 code

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'WikiSearch' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for WikiSearch
+  pod 'Alamofire','~> 4.7'
+  pod 'SwiftyJSON','~> 4.0'
+
+
+end
---
+++ WikiSearch/SearchResultsTableViewController.swift
@@ -0,0 +1,179 @@
+//
+//  SearchResultsTableViewController.swift
+//  WikiSearch
+//
+//  Created by user164669 on 2/9/20.
+//  Copyright © 2020 sachith. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+import Alamofire
+import SafariServices
+
+class SearchResultsTableViewController: UITableViewController {
+
+    private var searchController = UISearchController(searchResultsController: nil)
+    
+        private var searchResults = [JSON]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    private let apiFetcher = APIRequestFetcher()
+    private var previousRun = Date()
+    private let minInterval = 0.05
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+        setupTableViewBackgroundView()
+        setupSearchBar()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath ) -> CGFloat {
+        // #warning Incomplete implementation, return the number of rows
+        return 170
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        print(searchResults.count)
+        return searchResults.count
+    }
+    private func setupSearchBar() {
+        searchController.searchBar.delegate = self as! UISearchBarDelegate
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "Search any Topic"
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
+    }
+    private func setupTableViewBackgroundView() {
+       let backgroundViewLabel = UILabel(frame: .zero)
+       backgroundViewLabel.textColor = .darkGray
+       backgroundViewLabel.numberOfLines = 0
+       backgroundViewLabel.text =
+              "Oops, \n No results to show! ..."
+       tableView.backgroundView = backgroundViewLabel
+   }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",
+                                                 for: indexPath) as! CustomTableViewCell
+        
+        cell.titleLabel.text = searchResults[indexPath.row]["title"].stringValue
+        
+        cell.descriptionLabel.text = searchResults[indexPath.row]["terms"]["description"][0].string
+        
+        if let url = searchResults[indexPath.row]["thumbnail"]["source"].string {
+            apiFetcher.fetchImage(url: url, completionHandler: { image, _ in
+                cell.wikiImageView?.image = image
+            })
+        }
+        
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let title = searchResults[indexPath.row]["title"].stringValue
+        guard let url = URL.init(string: "https://en.wikipedia.org/wiki/\(title)")
+            else { return }
+        
+        let safariVC = SFSafariViewController(url: url)
+        present(safariVC, animated: true, completion: nil)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+   /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+   /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension SearchResultsTableViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchResults.removeAll()
+        guard let textToSearch = searchBar.text, !textToSearch.isEmpty else {
+            return
+        }
+        
+        if Date().timeIntervalSince(previousRun) > minInterval {
+            previousRun = Date()
+            fetchResults(for: textToSearch)
+        }
+    }
+    
+    func fetchResults(for text: String) {
+        print("Text Searched: \(text)")
+        apiFetcher.search(searchText: text, completionHandler: {
+            [weak self] results, error in
+            if case .failure = error {
+                return
+            }
+            
+            guard let results = results, !results.isEmpty else {
+                return
+            }
+            
+            self?.searchResults = results
+        })
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchResults.removeAll()
+    }
+}
